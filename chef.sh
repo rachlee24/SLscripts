a1d8a41d71ed3776c51c40cc2e6a7f086762236b
rachlee24 <leekinr@gmail.com>
2014-12-17 12:31:59 -0500

    Update chef.sh

--- chef.sh
+++ chef.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-
+echo "Provisioning VM"
+echo "Updating Linux"
+#This updates linux
 sudo apt-get update 
-
+#This upgrading linux
+echo "Upgrading Linux"
 sudo apt-get upgrade
 
+#This downloads the chef package
+echo "Getting chef package"
 wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.8-1.ubuntu.12.04_amd64.deb
 
+#installs the chef package
+echo "Installing chef package"
 sudo dpkg -i chef-server_11.0.8-1.ubuntu.12.04_amd64.deb
-
+#last step in configuring chef
+echo "Configuring chef package"
 sudo chef-server-ctl reconfigure
